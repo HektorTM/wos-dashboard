428b36dd2bee383027f678e4266849197eff3112
HektorTM <Paulvonberg.dev+github@gmail.com>
2025-10-07 00:30:51 +0200

    Fixes For dialog routes

--- backend/Routes/db_server/DialogRoutes.js
+++ backend/Routes/db_server/DialogRoutes.js
@@ -288,7 +288,7 @@ router.post('/:id/answer', async (req, res) => {
     const nextAnswerId = (maxIdResult[0].maxId || 0) + 1;
 
     try {
-        await db.query('INSERT INTO dialog_answers (dialog_id, answer_id, answer_text,  answer_action) VALUES (?,?,?,?,?) ', [id, nextAnswerId, answer_text, answer_action]);
+        await db.query('INSERT INTO dialog_answers (dialog_id, answer_id, answer_text,  answer_action) VALUES (?,?,?,?,?) ', [id, nextAnswerId, answer_text, answer_action ?? null]);
         res.status(200).json({message: 'Added Answer'});
     }  catch (err) {
         console.error(err.message);
@@ -310,12 +310,12 @@ router.delete('/:id/answer/:answerId', async (req, res) => {
 router.patch('/:id/answer/:answerId', async (req, res) => {
     const { id, answerId } = req.params;
     const { answer_text, answer_action } = req.body;
-    if (!answer_text || !answer_action) {
+    if (!answer_text) {
         return res.status(400).json({ error: 'Answer text and Answer action is required.' });
     }
 
     try {
-        await db.query('UPDATE dialog_answers SET answer_text = ?, answer_action = ? WHERE dialog_id = ? AND answer_id = ?', [answer_text, answer_action, id, answerId]);
+        await db.query('UPDATE dialog_answers SET answer_text = ?, answer_action = ? WHERE dialog_id = ? AND answer_id = ?', [answer_text , answer_action ?? null, id, answerId]);
         res.status(200).json({message: 'Updated Answer'});
     } catch (err) {
         console.error(err.message);
